6bb3b1129c92fd8ad5281eac41d8feb440539b09
ajcheon <cheon.andy@gmail.com>
2020-03-25 17:26:35 -0700

    Remove user defn, add pipeline.json

--- sparkle/user_definition.py
+++
@@ -1,6 +0,0 @@
-# https://developers.google.com/maps/documentation/javascript/get-api-key
-apikey = "AIzaSyCqtkDaTJE3ah__vjzeCyXq8ss3o-ovpR0"
-orig_coord = '37.7909,-122.3925'
-dest_coord = '37.7765,-122.4506'
-output_file = 'time.tsv'
-bucket_name="msds694"
