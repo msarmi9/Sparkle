6f3f0430361b18b889a6610b8d45540f941fbd6c
Collin Prather <collin.prather742@gmail.com>
2020-03-26 09:31:57 -0700

    yet another edit to test CodePipeline

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ def read_s3_obj(bucket_name, output_file):
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', message='Get ready to Sparkle and shine!!')
+    return render_template('index.html', message='Get ready to Sparkle and shine and fly!!')
 
 if __name__ == '__main__':
     application.jinja_env.auto_reload = True
