6f90d3aba2279add46eefddebd59ce8141218ce9
msarmi9 <msarmiento3@usfca.edu>
2020-04-14 08:53:15 -0700

    reformat with black

--- app/classes.py
+++ app/classes.py
@@ -3,10 +3,18 @@ from flask_login import UserMixin
 from flask_wtf.file import FileRequired
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
-from wtforms import FileField, PasswordField, StringField, SubmitField, IntegerField, TextAreaField
+from wtforms import (
+    FileField,
+    PasswordField,
+    StringField,
+    SubmitField,
+    IntegerField,
+    TextAreaField,
+)
 from wtforms.validators import DataRequired
 
 from app import db, login_manager
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,7 +23,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
-    patients = db.relationship('Patient', backref='user', lazy=True)
+    patients = db.relationship("Patient", backref="user", lazy=True)
 
     def __init__(self, firstname, lastname, username, email, password):
         self.firstname = firstname
@@ -30,6 +38,7 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), unique=False, nullable=False)
@@ -37,8 +46,9 @@ class Patient(db.Model):
     email = db.Column(db.String(80), unique=True, nullable=True)
     age = db.Column(db.Integer, unique=False, nullable=False)
     weight = db.Column(db.Integer, unique=False, nullable=False)
-    doctor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    prescriptions = db.relationship('Prescription', backref='patient', lazy=True)
+    doctor_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    prescriptions = db.relationship("Prescription", backref="patient", lazy=True)
+
 
 class Prescription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -49,11 +59,11 @@ class Prescription(db.Model):
     cycle_unit = db.Column(db.String(50), unique=False, nullable=False)
 
     # freq = number of times per cycle e.g. 2 (twice), 1 (once)
-    # cycle_n = every n <unit> 
+    # cycle_n = every n <unit>
     # cycle_unit = unit of repeat e.g. day, week, month
     # Example Rx: "twice daily" --> freq=2, cycle_n=1, cycle_unit=day
     # Example Rx: "once every other day" --> freq=1, cycle_n=2, cycle_unit=day
-    
+
     start_pills = db.Column(db.Integer, unique=False, nullable=False)
     remaining_pills = db.Column(db.Integer, unique=False, nullable=False)
     last_refill_date = db.Column(db.String(20), unique=False, nullable=False)
@@ -64,42 +74,47 @@ class Prescription(db.Model):
     # last_refill_date = when the Rx was last refilled (if first time, then date of first fill)
     # next_refill_date = when the Rx will be next refilled
 
-    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey("patient.id"), nullable=False)
+
 
 class PatientForm(FlaskForm):
-    firstname = StringField('First name', validators=[DataRequired()])
-    lastname = StringField('Last name', validators=[DataRequired()])
-    email = StringField('Email')
-    age = IntegerField('Age', validators=[DataRequired()])
-    weight = IntegerField('Weight', validators=[DataRequired()])
+    firstname = StringField("First name", validators=[DataRequired()])
+    lastname = StringField("Last name", validators=[DataRequired()])
+    email = StringField("Email")
+    age = IntegerField("Age", validators=[DataRequired()])
+    weight = IntegerField("Weight", validators=[DataRequired()])
 
 
 class PrescriptionForm(FlaskForm):
-    drug = StringField('Drug name', validators=[DataRequired()])
-    desc = StringField('Purpose / description', validators=[DataRequired()])
-    freq = IntegerField('Frequency', validators=[DataRequired()])
-    cycle_n = IntegerField('Cycle number', validators=[DataRequired()])
-    cycle_unit = StringField('Cycle unit', validators=[DataRequired()])
-    start_pills = IntegerField('Starting number of pills', validators=[DataRequired()])
+    drug = StringField("Drug name", validators=[DataRequired()])
+    desc = StringField("Purpose / description", validators=[DataRequired()])
+    freq = IntegerField("Frequency", validators=[DataRequired()])
+    cycle_n = IntegerField("Cycle number", validators=[DataRequired()])
+    cycle_unit = StringField("Cycle unit", validators=[DataRequired()])
+    start_pills = IntegerField("Starting number of pills", validators=[DataRequired()])
     # remaining_pills automatically populated in new_prescription route
-    last_refill_date = StringField('Latest refill date', validators=[DataRequired()])
+    last_refill_date = StringField("Latest refill date", validators=[DataRequired()])
+
 
 class RegistrationForm(FlaskForm):
-    firstname = StringField('First name', validators=[DataRequired()])
-    lastname = StringField('Last name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    firstname = StringField("First name", validators=[DataRequired()])
+    lastname = StringField("Last name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 class LogInForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
 
 class UploadFileForm(FlaskForm):
-    file_selector = FileField('File', validators=[FileRequired()])
-    submit = SubmitField('Submit')
+    file_selector = FileField("File", validators=[FileRequired()])
+    submit = SubmitField("Submit")
+
 
 db.create_all()
 db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -9,39 +9,42 @@ from io import StringIO
 
 
 # Home / splash page --------------------------------------------------------
-@application.route('/')
-@application.route('/home')
+@application.route("/")
+@application.route("/home")
 def index():
-    return render_template('splash.html', message='Welcome to Sparkle!')    
+    return render_template("splash.html", message="Welcome to Sparkle!")
+
 
 # Dashboard - visible once a user logs in -----------------------------------
-@application.route('/dashboard')
+@application.route("/dashboard")
 @login_required
 def dashboard():
     patients = User.query.filter_by(id=current_user.id).first().patients
-    return render_template('dashboard.html', patients=patients)
+    return render_template("dashboard.html", patients=patients)
 
 
 # Patient profile -----------------------------------------------------------
-@application.route('/patients/<int:patient_id>', methods=('GET', 'POST'))
+@application.route("/patients/<int:patient_id>", methods=("GET", "POST"))
 @login_required
 def patient_profile(patient_id):
     patient = Patient.query.filter_by(id=patient_id).first()
     prescriptions = patient.prescriptions
-    return render_template('patient_profile.html',
-                           patient=patient, prescriptions=prescriptions)
+    return render_template(
+        "patient_profile.html", patient=patient, prescriptions=prescriptions
+    )
 
 
 # New prescription for patient ----------------------------------------------
-@application.route('/patients/<int:patient_id>/new-prescription',
-                   methods=('GET', 'POST'))
+@application.route(
+    "/patients/<int:patient_id>/new-prescription", methods=("GET", "POST")
+)
 @login_required
 def add_prescription(patient_id):
     rx_form = PrescriptionForm()
     patient = Patient.query.filter_by(id=patient_id).first()
     if rx_form.validate_on_submit():
         drug = rx_form.drug.data
-        desc = rx_form.desc.data 
+        desc = rx_form.desc.data
         freq = rx_form.freq.data
         cycle_n = rx_form.cycle_n.data
         cycle_unit = rx_form.cycle_unit.data
@@ -49,11 +52,18 @@ def add_prescription(patient_id):
         last_refill_date = rx_form.last_refill_date.data
         remaining_pills = start_pills  # No pills taken yet at this point
 
-        rx = Prescription(drug=drug, desc=desc, freq=freq, cycle_n=cycle_n,
-                          cycle_unit=cycle_unit, start_pills=start_pills,
-                          last_refill_date=last_refill_date,
-                          remaining_pills=remaining_pills,
-                          next_refill_date='2020-05-10', patient=patient)
+        rx = Prescription(
+            drug=drug,
+            desc=desc,
+            freq=freq,
+            cycle_n=cycle_n,
+            cycle_unit=cycle_unit,
+            start_pills=start_pills,
+            last_refill_date=last_refill_date,
+            remaining_pills=remaining_pills,
+            next_refill_date="2020-05-10",
+            patient=patient,
+        )
 
         # TODO: latest, next refill dates should be date fields.
         # TODO: calculate next refill date based on freq, cycle_n, cycle_units
@@ -61,57 +71,63 @@ def add_prescription(patient_id):
 
         db.session.add(rx)
         db.session.commit()
-        return redirect(url_for('patient_profile', patient_id=patient_id))
-    return render_template('add_prescription.html',
-                           patient=patient, form=rx_form)
+        return redirect(url_for("patient_profile", patient_id=patient_id))
+    return render_template("add_prescription.html", patient=patient, form=rx_form)
 
 
 # New patient ---------------------------------------------------------------
-@application.route('/new-patient',  methods=('GET', 'POST'))
+@application.route("/new-patient", methods=("GET", "POST"))
 @login_required
 def add_patient():
     patient_form = PatientForm()
     if patient_form.validate_on_submit():
         firstname = patient_form.firstname.data
-        lastname = patient_form.lastname.data 
+        lastname = patient_form.lastname.data
         email = patient_form.email.data
         age = patient_form.age.data
         weight = patient_form.weight.data
 
-        patient = Patient(firstname=firstname, lastname=lastname, email=email,
-                          age=age, weight=weight, user=current_user)
+        patient = Patient(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            age=age,
+            weight=weight,
+            user=current_user,
+        )
         db.session.add(patient)
         db.session.commit()
-        return redirect(url_for('dashboard'))
-    return render_template('add_patient.html', form=patient_form)
+        return redirect(url_for("dashboard"))
+    return render_template("add_patient.html", form=patient_form)
 
 
 # New user registration -----------------------------------------------------
-@application.route('/register',  methods=('GET', 'POST'))
+@application.route("/register", methods=("GET", "POST"))
 def register():
     registration_form = RegistrationForm()
     if registration_form.validate_on_submit():
         firstname = registration_form.firstname.data
-        lastname = registration_form.lastname.data 
+        lastname = registration_form.lastname.data
         username = registration_form.username.data
         password = registration_form.password.data
         email = registration_form.email.data
 
-        user_count = User.query.filter_by(username=username).count() \
-                     + User.query.filter_by(email=email).count()
-        if (user_count > 0):
-            return '<h1>Error - Existing user : ' + username \
-                   + ' OR ' + email + '</h1>'
+        user_count = (
+            User.query.filter_by(username=username).count()
+            + User.query.filter_by(email=email).count()
+        )
+        if user_count > 0:
+            return "<h1>Error - Existing user : " + username + " OR " + email + "</h1>"
         else:
             user = User(firstname, lastname, username, email, password)
             db.session.add(user)
             db.session.commit()
-            return redirect(url_for('login'))
-    return render_template('register.html', form=registration_form)
+            return redirect(url_for("login"))
+    return render_template("register.html", form=registration_form)
 
 
 # User login ------------------------------------------------------------------
-@application.route('/login', methods=['GET', 'POST'])
+@application.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LogInForm()
     if login_form.validate_on_submit():
@@ -123,19 +139,19 @@ def login():
         # Login and validate the user; take them to dashboard page.
         if user is not None and user.check_password(password):
             login_user(user)
-            return redirect(url_for('dashboard'))
-    return render_template('login.html', form=login_form)
+            return redirect(url_for("dashboard"))
+    return render_template("login.html", form=login_form)
 
 
 # User logout ------------------------------------------------------------------
-@application.route('/logout')
+@application.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
 # File upload ---------------------------------------------------------------
-@application.route('/upload', methods=['GET', 'POST'])
+@application.route("/upload", methods=["GET", "POST"])
 def upload():
     """
     Upload a file from the client machine.
@@ -145,11 +161,12 @@ def upload():
         f = file.file_selector.data
         filename = f.filename
 
-        file_dir_path = os.path.join(application.instance_path, 'files')
+        file_dir_path = os.path.join(application.instance_path, "files")
         file_path = os.path.join(file_dir_path, filename)
         f.save(file_path)
-        return redirect(url_for('index'))
-    return render_template('upload.html', form=file)
+        return redirect(url_for("index"))
+    return render_template("upload.html", form=file)
+
 
 # Data receiving endpoint ---------------------------------------------------
 def adherence_model(data):
@@ -168,7 +185,7 @@ def adherence_model(data):
     return {"pred_string": pred_string, "pred": global_mean}
 
 
-@application.route('/send-data', methods=['POST'])
+@application.route("/send-data", methods=["POST"])
 def send_data():
     """
     Json in and json out
