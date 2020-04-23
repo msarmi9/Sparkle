5096bf3a5f5a445f5e10c946c43bb1519acafe05
msarmi9 <msarmiento3@usfca.edu>
2020-04-22 19:39:48 -0700

    enforce black code style

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.urandom(33)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://root:sparkleandshine@sparkle-042220.cpika6vye4g6.us-west-2.rds.amazonaws.com/postgres'
+    SQLALCHEMY_DATABASE_URI = "postgresql://root:sparkleandshine@sparkle-042220.cpika6vye4g6.us-west-2.rds.amazonaws.com/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
