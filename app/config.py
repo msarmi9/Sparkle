358bc2791d0de78f7c6d0274bf0c1faf2dee0386
msarmi9 <msarmiento3@usfca.edu>
2020-05-03 11:34:06 -0700

    rm flask env and config files

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SECRET_KEY = os.urandom(33)
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "sparkle.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
