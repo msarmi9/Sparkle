82d7df314150ab8b1c47421e1dd223218874f76f
msarmi9 <msarmiento3@usfca.edu>
2020-05-19 02:31:26 -0700

    add TestConfig class

--- sparkle/config.py
+++ sparkle/config.py
@@ -1,9 +1,19 @@
 import os
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    """Base class for Flask configuration."""
+
     SECRET_KEY = os.urandom(33)
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "sparkle.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class TestConfig(Config):
+    """Configure Flask app for running tests."""
+
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
