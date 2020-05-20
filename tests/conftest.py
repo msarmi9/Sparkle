b0aeb725ac85832b166322083bed9dd4f7ece328
msarmi9 <msarmiento3@usfca.edu>
2020-05-19 23:07:09 -0700

    use session-scoped app and function-scoped test_client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
+"""
+Configure common fixtures to be shared across all test modules.
+"""
 import pytest
 
 from sparkle import create_app
 from sparkle import db
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def app():
     """Configure a new instance of a Flask application for testing."""
     app = create_app()
     app.config.from_object("sparkle.config.TestConfig")
     with app.app_context():
         db.create_all()
-        db.session.commit()
-    yield app
+        yield app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def client(app):
     """Return a test client for the app."""
     return app.test_client()
