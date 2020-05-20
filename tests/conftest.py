2471f9f729f1c43ca3f45c4ed3a0e7b816f792ca
msarmi9 <msarmiento3@usfca.edu>
2020-05-20 16:18:08 -0700

    add fixtures for creating users and adding them to the db

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 
 from app import create_app
 from app import db
+from app.models.persons import User
 
 
 @pytest.fixture
@@ -21,3 +22,25 @@ def app():
 def client(app):
     """Return a test client for the app."""
     return app.test_client()
+
+
+@pytest.fixture
+def user_data():
+    """Return data for adding a sample user (doctor) to the database."""
+    keys = ["firstname", "lastname", "username", "email", "password"]
+    values = ["Anne", "Bronte", "annebronte", "anne@me.com", "emily"]
+    user_data = dict(zip(keys, values))
+    return user_data
+
+
+@pytest.fixture
+def user(user_data):
+    """Return an instance of the ``models.persons.User`` class."""
+    return User(**user_data)
+
+
+@pytest.fixture
+def init_user(user):
+    """Add a sample user (doctor) to the database."""
+    db.session.add(user)
+    db.session.commit()
