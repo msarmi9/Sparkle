0c843cc99c961a5c661955d6c27ca64d02b0bb2c
msarmi9 <msarmiento3@usfca.edu>
2020-05-22 02:40:53 -0700

    add tests for adherence dashboard

---
+++ tests/views/test_dashboard.py
@@ -0,0 +1,24 @@
+"""
+Tests dashboard view displaying adherence statistics.
+"""
+from tests.utils import captured_templates
+
+
+class TestDashboard:
+    """Tests adherence dashboard renders for logged in users only."""
+
+    def test_dashboard_requires_login(self, client):
+        """Non-logged in users cannot view adherence dashboard."""
+        response = client.get("/dashboard")
+        assert response.status_code == 401
+
+    def test_dashboard_view_no_rxs(self, app, client, login_user):
+        """Dashboard view renders with no prescriptions."""
+        with captured_templates(app) as templates:
+            response = client.get("/dashboard")
+            template, context = templates[0]
+            assert response.status_code == 200
+            assert type(context["adh_over_time"]) is str
+            assert type(context["top_general_adh"]) is str
+            assert type(context["top_ontime_adh"]) is str
+            assert template.name == "dashboard/dashboard.html"
