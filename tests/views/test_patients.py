a4199c1c6baddc3fc308dc977cb7513ac84c5bf1
msarmi9 <msarmiento3@usfca.edu>
2020-05-22 02:00:59 -0700

    add tests for add_patient view

--- tests/views/test_patients.py
+++ tests/views/test_patients.py
@@ -8,13 +8,14 @@ from dateutil.relativedelta import relativedelta
 
 from app import db
 from app.models.medication import Intake
+from app.models.persons import Patient
 from tests.utils import captured_templates
 
 
 views = [
     "/patients",
-    "/patients_deviating",
     "/patients_unprescribed",
+    "/patients_deviating",
     "/patients_ontrack",
 ]
 
@@ -35,18 +36,18 @@ def init_perfect_patient(rx_current, intake, patient, init_patient):
     db.session.commit()
 
 
-class TestAllPatientViews:
-    """Views for all, deviating, ontrack, and unprescribed patients are rendered."""
+class TestPatientViews:
+    """Tests for views for all, unprescribed, deviating, and ontrack patients."""
 
     @pytest.mark.parametrize("view", views)
     def test_patient_view_requires_login(self, client, view):
-        """Users (doctors) cannot view patient data when not logged in."""
+        """All patient views require users (doctors) to be logged in."""
         response = client.get(view)
         assert response.status_code == 401
 
     @pytest.mark.parametrize("view", views)
     def test_empty_view_if_no_patients(self, app, client, login_user, view):
-        """Empty patient view is rendered if there are no patients."""
+        """Patient view is empty if there are no patients."""
         with captured_templates(app) as templates:
             response = client.get(view)
             template, context = templates[0]
@@ -82,3 +83,36 @@ class TestAllPatientViews:
             assert context["rx_adherence"] == 100
             assert all(map(lambda p: p.is_adherent(), context["patients"]))
             assert template.name == "patients/patients_ontrack.html"
+
+    def test_patient_profile(self, app, client, login_user, init_perfect_patient):
+        """Detail view for single patient view is rendered."""
+        with captured_templates(app) as templates:
+            response = client.get("/patients/1")
+            template, context = templates[0]
+            assert response.status_code == 200
+            assert context["patient"].id == 1
+            assert context["prescriptions"][0].drug == "Vitamin C++"
+            assert template.name == "patients/patient_profile.html"
+
+
+class TestAddPatient:
+    """Tests for adding new patients to the database."""
+
+    def test_add_patient_view(self, app, client, login_user):
+        """View for adding new patients renders when requesting ``/new-patient``."""
+        with captured_templates(app) as templates:
+            response = client.get("/new-patient")
+            template, context = templates[0]
+            assert response.status_code == 200
+            assert template.name == "patients/add_patient.html"
+
+    def test_add_new_patient(self, app, client, login_user, patient_data):
+        """New patient is added to the db."""
+        with captured_templates(app) as templates:
+            r = client.post("/new-patient", data=patient_data, follow_redirects=True)
+            template, context = templates[0]
+            patients = context["current_user"].patients
+            assert r.status_code == 200
+            assert len(patients) == 1
+            assert patients[0].firstname == patient_data["firstname"]
+            assert template.name == "patients/patients.html"
