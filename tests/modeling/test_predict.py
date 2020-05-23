810dd70c2f6704ffc1228031333ba608466eb091
msarmi9 <msarmiento3@usfca.edu>
2020-05-23 10:51:28 -0700

    add test for classifying non-pill-taking activity

--- tests/modeling/test_predict.py
+++ tests/modeling/test_predict.py
@@ -9,16 +9,16 @@ import pytest
 from modeling.predict import adherence_model
 
 
-# Paths to training sets of recorded pill-taking data
-path_train_01 = Path("tests/data/2020-04-21_07_51_17_1.csv")
-path_train_02 = Path("tests/data/2020-04-27_21_00_19_2.csv")
-train_paths = [path_train_01, path_train_02]
+# Paths to training records of pill-taking data
+train_path_01 = Path("tests/data/train/2020-04-21_07_51_17_1.csv")
+train_path_02 = Path("tests/data/train/2020-04-27_21_00_19_2.csv")
+train_paths = [train_path_01, train_path_02]
 
-# Paths to holdout sets of recorded pill-taking data
-path_holdout = Path("")
+# Paths to holdout records of pill-taking data
+test_path_01 = Path("")
 
 # Paths to non-pill taking activities
-path_at_rest = Path("tests/data/2020-04-21_09_11_37_at_rest.csv")
+path_at_rest = Path("tests/data/non-pill/2020-04-21_09_11_37_at_rest.csv")
 
 
 # Adherence model requires string data, since that's how data is streamed from the watch
@@ -33,19 +33,27 @@ def get_label_from_path(csv_path):
     return int(csv_path.name.rstrip(".csv").split("_")[-1])
 
 
-@pytest.mark.parametrize("train_path", train_paths)
 class TestAdherenceModel:
     """Tests model can both learn from training data and predict well on unseen data."""
 
+    @pytest.mark.parametrize("train_path", train_paths)
     def test_classifier_on_training_data(self, train_path):
         """Model classifies activity as pill-taking for recorded pill intake data."""
         data = get_csv_string(train_path)
         pred_dict = adherence_model(data)
         assert pred_dict["pred_type"] == "regression"
 
+    @pytest.mark.parametrize("train_path", train_paths)
     def test_regressor_on_training_data(self, train_path, mae_tol=1):
         """Model predicts number of pills remaining within a given MAE tol."""
         data = get_csv_string(train_path)
         label = get_label_from_path(train_path)
         pred_dict = adherence_model(data)
         assert np.abs(pred_dict["pred"] - label) <= mae_tol
+
+    def test_classifier_on_data_at_rest(self):
+        """Classifier classifies data of a hand at rest as non-pill-taking activity."""
+        data = get_csv_string(path_at_rest)
+        pred_dict = adherence_model(data)
+        assert pred_dict["pred"] == 0
+        assert pred_dict["pred_type"] == "classification"
